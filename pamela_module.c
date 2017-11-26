330559b7ad0912d83792795fbb900527f1cced51
andre <maxime.andre@epitech.eu>
2017-11-26 20:06:13 +0100

    open-close-session

--- pamela_module.c
+++ pamela_module.c
@@ -10,18 +10,67 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 /* PAM entry point for session creation */
 int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-  printf("%s\n", "coucou");
+
+  const char  *user;
+  const char  *pass;
+  int         val;
+  char        cmd[4096];
+  char        *pathContainer = "/pamela/container";
+  char        *pathContainerFile;
+  char        *nameDevice;
+  char        *mountDevice;
+  char        *mountDest;
+  DIR         *dir;
 
   //recuperer login et mdp et decrypter conteneur grace a ca (et le monter je crois)
+  if ((val = pam_get_user(pamh, &user, "Username: ")) != PAM_SUCCESS)
+    return (val);
+  if ((val = pam_get_data(pamh, "pamela_user_pass", (const void **)&pass)) != PAM_SUCCESS) // ESSAYER AVEC PAM_AUTHTOK
+    pass = NULL;
+
+  //creation path
+  sprintf(pathContainerFile, "%s/%s", pathContainer, user);
+  sprintf(nameDevice, "device-%s", user);
+  sprintf(mountDevice, "/dev/mapper/device-%s", user);
+  sprintf(mountDest, "/home/%s/secure_data-rw", user);
+
+  // sh pour ouvrir et monter conteneur
+  dir = opendir(mountDest);
+  if (dir) {
+    sprintf(cmd, "echo '%s' | sudo cryptsetup luksOpen %s device-%s && sudo mount %s %s && sudo chown %s:users %s -R",
+            pass, pathContainerFile, user, mountDevice, mountDest, user, mountDest);
+  }
+  else {
+    sprintf(cmd, "sudo mkdir -p /pamela && sudo mkdir -p /pamela/container && fallocate -l 500MB %s && echo '%s' | sudo cryptsetup luksFormat %s && echo '%s' | sudo cryptsetup luksOpen %s device-%s && sudo mkfs.ext3 %s && mkdir %s && sudo mount %s %s && sudo chown %s:users %s -R",
+            pathContainerFile, pass, pathContainerFile, pass, pathContainerFile, user, nameDevice, mountDest, mountDevice, mountDest, user, mountDest);
+  }
+  system(cmd);
 
   return(PAM_SUCCESS);
 }
 
 /* PAM entry point for session cleanup */
 int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+  char        cmd[4096];
+  char        *nameDevice;
+  char        *mountDest;
+  const char        *user;
+  int         val;
+
+  if ((val = pam_get_user(pamh, &user, "Username: ")) != PAM_SUCCESS)
+    return (val);
+  sprintf(nameDevice, "device-%s", user);
+  sprintf(mountDest, "/home/%s/secure_data-rw", user);
+
+  sprintf(cmd, "sudo umount %s && sudo cryptsetup luksClose %s", mountDest, nameDevice);
+  system(cmd);
   return(PAM_SUCCESS);
 
   //log et mdp ET crypter conteneur et demonter
@@ -32,21 +81,29 @@ int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
   return(PAM_SUCCESS);
 }
 
+void my_cleanup(pam_handle_t *pamh, void *data, int error_status) {
+
+  if (data != NULL) {
+    bzero(data, strlen(data));
+    free(data);
+  }
+}
+
 /* PAM entry point for authentication verification */
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
 
   //log et mdp ET decrypter conteneur (creer si y'a pas)
 
-  const char *user = NULL;
-  int pgu_ret;
+  const char  *user;
+  const char  *pass;
+  int   val;
 
-  printf("%s\n", "salut");
-  pgu_ret = pam_get_user(pamh, &user, NULL);
-  if (pgu_ret != PAM_SUCCESS || user == NULL) {
-          return(PAM_SUCCESS);
-  }
-  printf("USER : %s\n", user);
-
+  if ((val = pam_get_user(pamh, &user, "Username: ")) != PAM_SUCCESS)
+    return (val);
+  if ((val = pam_get_item(pamh, PAM_AUTHTOK, (const void **)&pass)) != PAM_SUCCESS)
+    return (val);
+  if ((val = pam_set_data(pamh, "pamela_user_pass", strdup(pass), &my_cleanup)) != PAM_SUCCESS)
+    return (val);
   return(PAM_SUCCESS);
 }
 
@@ -60,7 +117,13 @@ int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
 
 /* PAM entry point for authentication token (password) changes */
 int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-
   // recuperer ancien mdp, decrypter conteneur, puis reencrypter avec nouveau mdp
+
+  const char  *oldPass;
+  const char  *pass;
+  int         val;
+
+  if ((val = pam_get_item(pamh, PAM_AUTHTOK, (const void **)&pass)) != PAM_SUCCESS)
+    return (val);
   return(PAM_SUCCESS);
 }
